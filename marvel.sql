20fc894654fdd132d08d337ad7cdc1ca617b8bd3
Ben Hamilton <ben.hamilton@me.com>
2019-04-15 15:49:10 +0100

    updated Krusty the Clowns name

--- marvel.sql
+++ marvel.sql
@@ -50,7 +50,10 @@ INSERT INTO movies (title, year, show_time) VALUES ('Spider-Man: Homecoming', 20
 INSERT INTO movies (title, year, show_time) VALUES ('Thor: Ragnarok', 2017, '22:10');
 INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:00');
 
+UPDATE people SET name = 'Krusty the Clown' WHERE id = 13;
 
 SELECT * FROM movies;
+
+SELECT * FROM people;
 
 SELECT name FROM people;
